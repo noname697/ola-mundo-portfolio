79bef417b7de0d55842794dca5c2a381801f1435
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-21 17:58:32 -0300

    Começando componente de post

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,9 +1,22 @@
 import { useParams } from "react-router-dom";
+import posts from "../../json/posts.json";
+import PostModelo from "componentes/PostModelo/PostModelo";
 
 const Post = () => {
   const { id } = useParams();
 
-  return <h1>Post {id}</h1>;
+  const post = posts.find((post) => {
+    return post.id === Number(id);
+  });
+
+  return (
+    <PostModelo
+      fotoCapa={`/assets/posts/${post.id}/capa.png`}
+      titulo={post.titulo}
+    >
+      {post.texto}
+    </PostModelo>
+  );
 };
 
 export default Post;
