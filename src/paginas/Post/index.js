98ce3f7bf93340f72098d6117b7827d545bdfcde
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-21 19:23:49 -0300

    Posts Recomendados

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -5,6 +5,7 @@ import ReactMarkdown from "react-markdown";
 import "./Post.css";
 import NaoEncontrada from "paginas/NaoEncontrada";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import PostCard from "componentes/PostCard";
 
 const Post = () => {
   const { id } = useParams();
@@ -17,20 +18,43 @@ const Post = () => {
     return <NaoEncontrada />;
   }
 
+  const index = posts.findIndex((post) => post.id === Number(id));
+  let postsRecomendados = [...posts];
+  postsRecomendados.splice(index, 1);
+  postsRecomendados = postsRecomendados.slice(0, 4);
+
   return (
     <Routes>
       <Route path="*" element={<PaginaPadrao />}>
         <Route
           index
           element={
-            <PostModelo
-              fotoCapa={`/assets/posts/${post.id}/capa.png`}
-              titulo={post.titulo}
-            >
-              <div className="post-markdown-container">
-                <ReactMarkdown>{post.texto}</ReactMarkdown>
+            <>
+              <PostModelo
+                fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                titulo={post.titulo}
+              >
+                <div className="post-markdown-container">
+                  <ReactMarkdown>{post.texto}</ReactMarkdown>
+                </div>
+              </PostModelo>
+              <div className="recomendados-container">
+                <h1>Outros posts que você pode gostar: </h1>
+                <ul className="lista-recomendados">
+                  {postsRecomendados.map((post) => {
+                    if (post.id !== Number(id)) {
+                      return (
+                        <li key={post.id}>
+                          <PostCard post={post} />
+                        </li>
+                      );
+                    } else {
+                      return "";
+                    }
+                  })}
+                </ul>
               </div>
-            </PostModelo>
+            </>
           }
         />
       </Route>
